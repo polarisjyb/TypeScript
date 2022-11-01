1296f6c56de918b94db1f7c5e2328095587e543f
Violet Ultra <evia_a@naver.com>
2022-11-01 17:50:15 +0900

    타입스크립트 기초 연습

---
+++ test-1.js
@@ -0,0 +1,6 @@
+"use strict";
+function practice(a, b) {
+    return a + b;
+}
+;
+console.log(practice('노드', '익스프레스'));
---
+++ test-1.ts
@@ -0,0 +1,5 @@
+function practice(a: string, b: string) {
+  return a+b;
+};
+
+console.log(practice('노드', '익스프레스'));
